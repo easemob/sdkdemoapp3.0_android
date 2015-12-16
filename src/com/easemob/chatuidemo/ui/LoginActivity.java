ff9a4ca7400362430673c2984233a9f3671645d3
zhaoliang <zhaoliang@easemob.com>
2015-12-16 16:31:53 +0800

    Call logout() when user cancel login. Fix ANDROID-114

--- src/com/easemob/chatuidemo/ui/LoginActivity.java
+++ src/com/easemob/chatuidemo/ui/LoginActivity.java
@@ -118,6 +118,8 @@ public class LoginActivity extends BaseActivity {
 
 			@Override
 			public void onCancel(DialogInterface dialog) {
+				Log.d(TAG, "onCancel, call logout");
+				EMClient.getInstance().logout();
 				progressShow = false;
 			}
 		});
