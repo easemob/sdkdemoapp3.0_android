7de1634bdd1c2bb3ce7f0b8799e18de8f7533680
linan <greenmemo@163.com>
2015-12-28 17:40:05 +0800

    downloading img, account got kicked, crash
    http://172.16.0.32:8080/browse/ANDROID-161

--- src/com/hyphenate/chatuidemo/ui/MainActivity.java
+++ src/com/hyphenate/chatuidemo/ui/MainActivity.java
@@ -470,7 +470,9 @@ public class MainActivity extends BaseActivity {
 						dialog.dismiss();
 						conflictBuilder = null;
 						finish();
-						startActivity(new Intent(MainActivity.this, LoginActivity.class));
+						Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+						intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+						startActivity(intent);
 					}
 				});
 				conflictBuilder.setCancelable(false);
