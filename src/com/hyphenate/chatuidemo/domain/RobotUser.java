0403308164f453ffd43b51d1bffab5ba6e4da910
zhaoliang <zhaoliang@easemob.com>
2016-04-01 14:44:07 +0800

    use lower case for robot username.

--- src/com/hyphenate/chatuidemo/domain/RobotUser.java
+++ src/com/hyphenate/chatuidemo/domain/RobotUser.java
@@ -17,6 +17,6 @@ import com.hyphenate.easeui.domain.EaseUser;
 
 public class RobotUser extends EaseUser{
 	public RobotUser(String username) {
-        super(username);
+        super(username.toLowerCase());
     }
 }
