90a9c34255b2bc8d3b964fa15c89bcf363477cd6
sunyujia <sunyujia0227@hotmail.com>
2016-05-10 15:10:44 +0800

    重命名包命为ui库的包命，解决资源文件找不到的问题

--- redpacketlibrary/src/androidTest/java/com/easemob/luckymoneyui/ApplicationTest.java
+++ redpacketlibrary/src/androidTest/java/com/easemob/luckymoneyui/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.easemob.redpacketlibrary;
+package com.easemob.luckymoneyui;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- redpacketlibrary/src/test/java/com/easemob/luckymoneyui/ExampleUnitTest.java
+++ redpacketlibrary/src/test/java/com/easemob/luckymoneyui/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.easemob.redpacketlibrary;
+package com.easemob.luckymoneyui;
 
 import org.junit.Test;
 
