b8ad1f0195a28e74feacc42287890b7aa32d2bff
linan <linan@easemob.com>
2016-12-21 15:48:17 +0800

    api refactor: message -> msg

--- src/com/hyphenate/chatuidemo/ui/OfflinePushNickActivity.java
+++ src/com/hyphenate/chatuidemo/ui/OfflinePushNickActivity.java
@@ -44,7 +44,7 @@ public class OfflinePushNickActivity extends BaseActivity {
 					@Override
 					public void run() {
 						// TODO Auto-generated method stub
-						boolean updatenick = EMClient.getInstance().pushManager().updateCurrentUserNick(
+						boolean updatenick = EMClient.getInstance().pushManager().updateCurrentUserNickname(
 								inputNickName.getText().toString());
 						if (!updatenick) {
 							runOnUiThread(new Runnable() {
